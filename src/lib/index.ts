5177e8129b2e4bd1fc25a56e766fd650e61beff2
Khaliq Gant <khaliqgant@gmail.com>
2018-08-14 14:52:08 +0200

    add in interface and docblocks

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,20 @@
+import { ForkCall } from '../interfaces/SimpleFork';
 const fork = require('./fork');
 
+/**
+ *
+ * Simple Fork
+ * @desc take in a script path and setup a fork that can call a method
+ * once a forked process has been created
+ *
+ */
 export class SimpleFork {
 
   private _path: string;
 
   private _fnName: string;
 
-  private _call: any = {};
+  private _call: ForkCall;
 
   private _forkedProcess: any;
 
@@ -16,6 +24,13 @@ export class SimpleFork {
 
   }
 
+  /**
+   *
+   * Fork
+   * @desc store the function name that we want to execute in a forked process
+   * and setup a fork that will be executed by the caller later
+   *
+   */
   public fork(fnName: string, ...args: any[]): Function {
 
     this._fnName = fnName;
@@ -25,13 +40,27 @@ export class SimpleFork {
 
   }
 
+  /**
+   *
+   * End
+   * @desc stop the fork process
+   *
+   */
   public end() {
 
     this.stopChild();
 
   }
 
-  private setup() {
+  /**
+   *
+   * Setup
+   * @desc return a function for the caller to call to call a method within
+   * the forked process. Start the child process and send the call information
+   * needed to run the method when called
+   *
+   */
+  private setup(): Function {
 
     return (...args: any[]) => {
       const fnArgs = args.slice(0);
@@ -46,12 +75,24 @@ export class SimpleFork {
 
   }
 
+  /**
+   *
+   * Send
+   * @desc send the call data to the child forked process
+   *
+   */
   private send() {
 
     this._forkedProcess.send(this._call);
 
   }
 
+  /**
+   *
+   * Start Child
+   * @desc instantiate a forked process via a wrapper around the fork call
+   *
+   */
   private startChild() {
 
     this._forkedProcess = fork.run(this._path, {
@@ -64,6 +105,13 @@ export class SimpleFork {
 
   }
 
+  /**
+   *
+   * Stop Child
+   * @desc send a die command to the forked process and send a kill signal
+   * as well if that is not processed
+   *
+   */
   private stopChild() {
 
     this._forkedProcess.send('die');
@@ -76,6 +124,13 @@ export class SimpleFork {
 
   }
 
+  /**
+   *
+   * Receive
+   * @desc receive any data sent from the caller and apply it to the
+   * callback function
+   *
+   */
   private receive(data: any) {
 
     const { args } = data;
@@ -90,7 +145,7 @@ export class SimpleFork {
 
   }
 
-  private exit(code: any) {
+  private exit(code: number) {
 
     this.stopChild();
 
